991848d595e6c8ad0e7101338a502a2dedf0eb81
Giancarlos Salas <giansalex@gmail.com>
2022-02-25 14:04:38 -0500

    Add osmosis proto types

--- packages/osmo-proto/src/lib.rs
+++ packages/osmo-proto/src/lib.rs
@@ -1,2 +1,14 @@
 pub mod proto_ext;
 pub mod query;
+
+/// The version (commit hash) of the Osmosis SDK used when generating this library.
+pub const OSMOSIS_VERSION: &str = include_str!("types/OSMOSIS_COMMIT");
+
+pub mod osmosis {
+    pub mod gamm {
+        pub mod v1beta1 {
+            include!("types/osmosis.gamm.v1beta1.rs");
+            include!("paths/osmosis.gamm.v1beta1.rs");
+        }
+    }
+}
