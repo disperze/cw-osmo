ee763ab2166044e24ed7e5047bb5b2b43dfb13a4
Giancarlos Salas <giansalex@gmail.com>
2022-02-27 10:55:32 -0500

    Add estimate swap query

--- contracts/ibc-query/src/ibc_msg.rs
+++ contracts/ibc-query/src/ibc_msg.rs
@@ -6,7 +6,10 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum PacketMsg {
+    /// gamm/SpotPrice
     SpotPrice(SpotPricePacket),
+    /// gamm/EstimateSwapExactAmountIn
+    EstimateSwapAmountIn(EstimateSwapAmountInPacket),
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -14,6 +17,20 @@ pub struct SpotPricePacket {
     pub pool_id: Uint64,
     pub token_in: String,
     pub token_out: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct EstimateSwapAmountInPacket {
+    pub sender: String,
+    pub pool_id: Uint64,
+    pub token_in: String,
+    pub routes: Vec<SwapAmountInRoute>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct SwapAmountInRoute {
+    pub pool_id: Uint64,
+    pub token_out_denom: String,
 }
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
@@ -28,4 +45,13 @@ pub enum PacketAck {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct SpotPriceAck {
     pub price: String,
+}
+
+impl From<SwapAmountInRoute> for cw_osmo_proto::osmosis::gamm::v1beta1::SwapAmountInRoute  {
+    fn from(msg: SwapAmountInRoute) -> cw_osmo_proto::osmosis::gamm::v1beta1::SwapAmountInRoute {
+        cw_osmo_proto::osmosis::gamm::v1beta1::SwapAmountInRoute {
+            pool_id: msg.pool_id.into(),
+            token_out_denom: msg.token_out_denom,
+        }
+    }
 }
--- packages/osmo-proto/src/paths/osmosis.gamm.v1beta1.rs
+++ packages/osmo-proto/src/paths/osmosis.gamm.v1beta1.rs
@@ -6,6 +6,12 @@ impl ProtoUrl for QuerySpotPriceRequest {
     }
 }
 
+impl ProtoUrl for QuerySwapExactAmountInRequest {
+    fn path(&self) -> String {
+        String::from("/osmosis.gamm.v1beta1.Query/EstimateSwapExactAmountIn")
+    }
+}
+
 impl ProtoUrl for MsgSwapExactAmountIn {
     fn path(&self) -> String {
         String::from("/osmosis.gamm.v1beta1.MsgSwapExactAmountIn")
