e80fd33714743d9d65fbeba38f7f7aa1624f5cbd
Giancarlos Salas <giansalex@gmail.com>
2022-04-02 16:34:21 -0500

    Add nonpayable error + tests

--- contracts/lockup/src/error.rs
+++ contracts/lockup/src/error.rs
@@ -1,5 +1,6 @@
 use cosmwasm_std::StdError;
 use cw_controllers::AdminError;
+use cw_utils::PaymentError;
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq)]
@@ -9,6 +10,9 @@ pub enum ContractError {
 
     #[error("{0}")]
     Admin(#[from] AdminError),
+
+    #[error("{0}")]
+    Payment(#[from] PaymentError),
 
     #[error("Empty balance")]
     NoBalance {},
