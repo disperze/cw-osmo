26d7180e3ca3aef1a453680a8ccb42b8ee6f5cb8
Giancarlos Salas <giansalex@gmail.com>
2022-04-03 16:16:39 -0500

    Events mock update

--- contracts/ics20-swap/src/test_helpers.rs
+++ contracts/ics20-swap/src/test_helpers.rs
@@ -8,7 +8,7 @@ use cosmwasm_std::testing::{
     mock_dependencies, mock_env, mock_ibc_channel_connect_ack, mock_ibc_channel_open_init,
     mock_info, MockApi, MockQuerier, MockStorage,
 };
-use cosmwasm_std::{Attribute, DepsMut, Event, IbcEndpoint, OwnedDeps};
+use cosmwasm_std::{attr, DepsMut, Event, IbcEndpoint, OwnedDeps};
 
 use crate::msg::InitMsg;
 
@@ -53,81 +53,75 @@ pub fn setup(channels: &[&str]) -> OwnedDeps<MockStorage, MockApi, MockQuerier> 
     deps
 }
 
-pub fn swap_events_mock() -> Vec<Event> {
-    let mut ev1 = Event::new("token_swapped");
-    ev1.attributes = vec![
-        Attribute::new("module", "gamm"),
-        Attribute::new("sender", "osmo1q4aw0vtcyyredprm4ncmr4jdj70kpgyr3"),
-        Attribute::new("pool_id", "497"),
-        Attribute::new(
-            "tokens_in",
-            "10000000ibc/46B44899322F3CD854D2D46DEEF881958467CDD4B3B10086DA49296BBED94BED",
-        ),
-        Attribute::new(
-            "tokens_out",
-            "338527564ibc/BE1BB42D4BE3C30D50B68D7C41DB4DFCE9678E8EF8C539F6E6A9345048894FCC",
-        ),
-        Attribute::new("module", "gamm"),
-        Attribute::new("sender", "osmo1q4aw0vtcyyredprm4ncmr4jdj70kpgyr3"),
-        Attribute::new("pool_id", "560"),
-        Attribute::new("tokens_in", "36601070uosmo"),
-        Attribute::new("tokens_out", "36601070uosmo"),
+pub fn mock_swap_events() -> Vec<Event> {
+    return vec![
+        Event::new("token_swapped").add_attributes(vec![
+            attr("module", "gamm"),
+            attr("sender", "osmo1q4aw0vtcyyredprm4ncmr4jdj70kpgyr3"),
+            attr("pool_id", "497"),
+            attr(
+                "tokens_in",
+                "10000000ibc/46B44899322F3CD854D2D46DEEF881958467CDD4B3B10086DA49296BBED94BED",
+            ),
+            attr(
+                "tokens_out",
+                "338527564ibc/BE1BB42D4BE3C30D50B68D7C41DB4DFCE9678E8EF8C539F6E6A9345048894FCC",
+            ),
+            attr("module", "gamm"),
+            attr("sender", "osmo1q4aw0vtcyyredprm4ncmr4jdj70kpgyr3"),
+            attr("pool_id", "560"),
+            attr("tokens_in", "36601070uosmo"),
+            attr("tokens_out", "36601070uosmo"),
+        ]),
+        Event::new("transfer").add_attributes(vec![
+            attr(
+                "recipient",
+                "osmo1h7yfu7x4qsv2urnkl4kzydgxegdfyjdry5ee4xzj98jwz0uh07rqdkmprr",
+            ),
+            attr("sender", "osmo1q4aw0vtcyyredprm4ncmr4jdj70kpgyr3"),
+            attr(
+                "amount",
+                "10000000ibc/46B44899322F3CD854D2D46DEEF881958467CDD4B3B10086DA49296BBED94BED",
+            ),
+            attr("recipient", "osmo1q4aw0vtcyyredprm4ncmr4jdj70kpgyr3"),
+            attr(
+                "sender",
+                "osmo10d8ddsydag5xrnl2kacmkjtdxddstvz4jvraqqpf6ss2n7fy6lkqw4sx2f",
+            ),
+            attr(
+                "amount",
+                "338527564ibc/BE1BB42D4BE3C30D50B68D7C41DB4DFCE9678E8EF8C539F6E6A9345048894FCC",
+            ),
+        ]),
     ];
-    let mut ev2 = Event::new("transfer");
-    ev2.attributes = vec![
-        Attribute::new(
-            "recipient",
-            "osmo1h7yfu7x4qsv2urnkl4kzydgxegdfyjdry5ee4xzj98jwz0uh07rqdkmprr",
-        ),
-        Attribute::new("sender", "osmo1q4aw0vtcyyredprm4ncmr4jdj70kpgyr3"),
-        Attribute::new(
-            "amount",
-            "10000000ibc/46B44899322F3CD854D2D46DEEF881958467CDD4B3B10086DA49296BBED94BED",
-        ),
-        Attribute::new("recipient", "osmo1q4aw0vtcyyredprm4ncmr4jdj70kpgyr3"),
-        Attribute::new(
-            "sender",
-            "osmo10d8ddsydag5xrnl2kacmkjtdxddstvz4jvraqqpf6ss2n7fy6lkqw4sx2f",
-        ),
-        Attribute::new(
-            "amount",
-            "338527564ibc/BE1BB42D4BE3C30D50B68D7C41DB4DFCE9678E8EF8C539F6E6A9345048894FCC",
-        ),
-    ];
-
-    return vec![ev1, ev2];
 }
 
-pub fn join_pool_events_mock() -> Vec<Event> {
-    let mut ev1 = Event::new("pool_joined");
-    ev1.attributes = vec![
-        Attribute::new("module", "gamm"),
-        Attribute::new("sender", "osmo1q4aw0vtcyyredprm4ncmr4jdj70kpgyr3"),
-        Attribute::new("pool_id", "1"),
-        Attribute::new("tokens_in", "10000000uosmo"),
+pub fn mock_join_pool_events() -> Vec<Event> {
+    return vec![
+        Event::new("pool_joined").add_attributes(vec![
+            attr("module", "gamm"),
+            attr("sender", "osmo1q4aw0vtcyyredprm4ncmr4jdj70kpgyr3"),
+            attr("pool_id", "1"),
+            attr("tokens_in", "10000000uosmo"),
+        ]),
+        Event::new("coinbase").add_attributes(vec![
+            attr("minter", "osmo1c9y7crgg6y9pfkq0y8mqzknqz84c3etr0kpcvj"),
+            attr("amount", "74196993097318119147gamm/pool/1"),
+        ]),
     ];
-    let mut ev2 = Event::new("coinbase");
-    ev2.attributes = vec![
-        Attribute::new("minter", "osmo1c9y7crgg6y9pfkq0y8mqzknqz84c3etr0kpcvj"),
-        Attribute::new("amount", "74196993097318119147gamm/pool/1"),
-    ];
-
-    return vec![ev1, ev2];
 }
 
-pub fn exit_pool_events_mock() -> Vec<Event> {
-    let mut ev1 = Event::new("pool_exited");
-    ev1.attributes = vec![
-        Attribute::new("module", "gamm"),
-        Attribute::new("sender", "osmo1q4aw0vtcyyredprm4ncmr4jdj70kpgyr3"),
-        Attribute::new("pool_id", "1"),
-        Attribute::new("tokens_out", "9970022uosmo"),
+pub fn mock_exit_pool_events() -> Vec<Event> {
+    return vec![
+        Event::new("pool_exited").add_attributes(vec![
+            attr("module", "gamm"),
+            attr("sender", "osmo1q4aw0vtcyyredprm4ncmr4jdj70kpgyr3"),
+            attr("pool_id", "1"),
+            attr("tokens_out", "9970022uosmo"),
+        ]),
+        Event::new("burn").add_attributes(vec![
+            attr("burner", "osmo1c9y7crgg6y9pfkq0y8mqzknqz84c3etr0kpcvj"),
+            attr("amount", "74196993097318119147gamm/pool/1"),
+        ]),
     ];
-    let mut ev2 = Event::new("burn");
-    ev2.attributes = vec![
-        Attribute::new("burner", "osmo1c9y7crgg6y9pfkq0y8mqzknqz84c3etr0kpcvj"),
-        Attribute::new("amount", "74196993097318119147gamm/pool/1"),
-    ];
-
-    return vec![ev1, ev2];
 }
